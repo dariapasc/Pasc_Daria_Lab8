ac6a67edf50635994de0df0b6fceae5215db286f
DARIA-NICOLA PASC <daria.pasc@stud.ubbcluj.ro>
2021-11-23 12:52:22 +0200

    Add project files.

---
+++ Data/Pasc_Daria_Lab8Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Pasc_Daria_Lab8.Models;
+
+namespace Pasc_Daria_Lab8.Data
+{
+    public class Pasc_Daria_Lab8Context : DbContext
+    {
+        public Pasc_Daria_Lab8Context (DbContextOptions<Pasc_Daria_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Pasc_Daria_Lab8.Models.Book> Book { get; set; }
+
+        public DbSet<Pasc_Daria_Lab8.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Pasc_Daria_Lab8.Models.Category> Category { get; set; }
+    }
+}
